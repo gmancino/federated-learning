215a731939e81b292362003c436ddc5364a3cc13
Gabe Mancino <gmancino.777@gmail.com>
2019-10-02 14:43:30 -0400

    Upload new FMNIST results

--- Framework1/NumericalExperiments/FashionTest.py
+++ Framework1/NumericalExperiments/FashionTest.py
@@ -38,33 +38,44 @@ print('Accuracy: ', sum(pred == test_label)/len(pred))
 # Save "true" values for comparison
 w_true = all_data_train.w
 b_true = all_data_train.b
-b_true_mean = b_true - np.mean(b_true)
+#b_true_mean = b_true - np.mean(b_true)
 
-w_true = np.concatenate((w_true, np.array([b_true_mean])), axis=0)
+w_true = np.concatenate((w_true, np.array([b_true])), axis=0)
 
 w_seq = []
 corruptions_seq = []
+accuracy = []
+num_test = 100
 
-for i in range(1, 16):
+for i in range(1, 21):
 
-    y_corrupt_ind = np.random.randint(len(test_label[0:100]), size=math.floor((i * 0.01) * len(test_label[0:10000])))
+    y_corrupt_ind = np.random.randint(len(test_label[0:num_test]), size=math.floor((i * 0.01) * len(test_label[0:num_test])))
     corruptions = np.random.randint(10, size=len(y_corrupt_ind))
-    y_corrupt = test_label[0:100].copy()
+    y_corrupt = test_label[0:num_test].copy()
     y_corrupt[y_corrupt_ind] = corruptions
 
-    corruption_log_reg = MulticlassLogReg(test_data[0:100], y_corrupt, np.random.rand(test_data.shape[1], 10), np.random.rand(10), T=1000, tol=0.5, alpha=0.25, lam1=1e-3, lam2=0.1, num_classes=10)
+    corruption_log_reg = MulticlassLogReg(test_data[0:num_test], y_corrupt, np.random.rand(test_data.shape[1], 10), np.random.rand(10), T=1000, tol=0.5, alpha=0.25, lam1=1e-3, lam2=0.1, num_classes=10)
     corruption_log_reg.optimization()
 
-    w = np.concatenate((corruption_log_reg.w, np.array([corruption_log_reg.b - np.mean(b_true)])), axis=0)
+    w = np.concatenate((corruption_log_reg.w, np.array([corruption_log_reg.b - np.mean(corruption_log_reg.b)])), axis=0)
 
     norm = np.linalg.norm(w - w_true, ord='fro')/np.linalg.norm(w_true, ord='fro')
 
+    pred, prob = all_data_train.predict(test_data[0:num_test], corruption_log_reg.w, corruption_log_reg.b)
+
+    acc = sum(pred == test_label[0:num_test]) / len(pred)
+
     corruptions_seq.append(i * 0.01)
     w_seq.append(norm)
+    accuracy.append(acc)
 
 
-fig = plt.figure()
+plt.subplot(211)
 plt.plot(corruptions_seq, w_seq)
-plt.title(str(len(test_label[0:100])) + ' data points')
-plt.xlabel('Percent corruptions')
+plt.title(str(len(test_label[0:num_test])) + ' data points on Fashion-MNIST data')
 plt.ylabel('||w_c-w_true||_F/||w_true||_F')
+plt.subplot(212)
+plt.plot(corruptions_seq, accuracy)
+plt.ylabel('Classification accuracy')
+plt.xlabel('Corruption percentages')
+plt.show()
