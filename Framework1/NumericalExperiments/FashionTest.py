09dfc0793c54641277347668595fcf0ab83f0cd1
Gabe Mancino <gmancino.777@gmail.com>
2019-10-03 09:46:45 -0400

    Update testing files to have same starting parameters

--- Framework1/NumericalExperiments/FashionTest.py
+++ Framework1/NumericalExperiments/FashionTest.py
@@ -27,8 +27,12 @@ for i in range(50000):
 for i in range(10000):
     test_data[i, :] = x_data[50000 + i][0].flatten()
 
+
+w0 = np.random.rand(train_data.shape[1], 10)
+b0 = np.random.rand(10)
+
 # Run multiclass logistic regression to get "w_true"
-all_data_train = MulticlassLogReg(train_data, train_label, np.random.rand(train_data.shape[1], 10), np.random.rand(10), T=1000, tol=0.5, alpha=0.25, lam1=1e-3, lam2=0.1, num_classes=10)
+all_data_train = MulticlassLogReg(train_data, train_label, w0, b0, T=1000, tol=0.5, alpha=0.25, lam1=1e-3, lam2=0.1, num_classes=10)
 all_data_train.optimization()
 
 pred, prob = all_data_train.predict(test_data, all_data_train.w, all_data_train.b)
@@ -38,23 +42,23 @@ print('Accuracy: ', sum(pred == test_label)/len(pred))
 # Save "true" values for comparison
 w_true = all_data_train.w
 b_true = all_data_train.b
-#b_true_mean = b_true - np.mean(b_true)
+b_true_mean = b_true - np.mean(b_true)
 
-w_true = np.concatenate((w_true, np.array([b_true])), axis=0)
+w_true = np.concatenate((w_true, np.array([b_true_mean])), axis=0)
 
 w_seq = []
 corruptions_seq = []
 accuracy = []
 num_test = 10000
 
-for i in range(90, 91):
+for i in range(1, 16):
 
     y_corrupt_ind = np.random.randint(len(test_label[0:num_test]), size=math.floor((i * 0.01) * len(test_label[0:num_test])))
     corruptions = np.random.randint(10, size=len(y_corrupt_ind))
     y_corrupt = test_label[0:num_test].copy()
     y_corrupt[y_corrupt_ind] = corruptions
 
-    corruption_log_reg = MulticlassLogReg(test_data[0:num_test], y_corrupt, np.random.rand(test_data.shape[1], 10), np.random.rand(10), T=1000, tol=0.5, alpha=0.25, lam1=1e-3, lam2=0.1, num_classes=10)
+    corruption_log_reg = MulticlassLogReg(test_data[0:num_test], y_corrupt, w0, b0, T=1000, tol=0.5, alpha=0.25, lam1=1e-3, lam2=0.1, num_classes=10)
     corruption_log_reg.optimization()
 
     w = np.concatenate((corruption_log_reg.w, np.array([corruption_log_reg.b - np.mean(corruption_log_reg.b)])), axis=0)
--- Framework1/NumericalExperiments/TestingFile.py
+++ Framework1/NumericalExperiments/TestingFile.py
@@ -24,8 +24,11 @@ test_data = x[50000:60000, :]
 train_label = y[0:50000]
 test_label = y[50000:60000]
 
+w0 = np.random.rand(x.shape[1], 10)
+b0 = np.random.rand(10)
+
 # Run multiclass logistic regression to get "w_true"
-all_data_train = MulticlassLogReg(train_data, train_label, np.random.rand(train_data.shape[1], 10), np.random.rand(10), T=1000, tol=0.5, alpha=0.25, lam1=1e-3, lam2=0.1, num_classes=10)
+all_data_train = MulticlassLogReg(train_data, train_label, w0, b0, T=1000, tol=0.5, alpha=0.25, lam1=1e-3, lam2=0.1, num_classes=10)
 all_data_train.optimization()
 
 pred, prob = all_data_train.predict(test_data, all_data_train.w, all_data_train.b)
@@ -42,16 +45,16 @@ w_true = np.concatenate((w_true, np.array([b_true_mean])), axis=0)
 w_seq = []
 corruptions_seq = []
 accuracy = []
-num_test = 1000
+num_test = 10000
 
-for i in range(1, 31):
+for i in range(1, 21):
 
     y_corrupt_ind = np.random.randint(len(test_label[0:num_test]), size=math.floor((i * 0.01) * len(test_label[0:num_test])))
     corruptions = np.random.randint(10, size=len(y_corrupt_ind))
     y_corrupt = test_label[0:num_test].copy()
     y_corrupt[y_corrupt_ind] = corruptions
 
-    corruption_log_reg = MulticlassLogReg(test_data[0:num_test], y_corrupt, np.random.rand(test_data.shape[1], 10), np.random.rand(10), T=1000, tol=0.5, alpha=0.25, lam1=1, lam2=1, num_classes=10)
+    corruption_log_reg = MulticlassLogReg(test_data[0:num_test], y_corrupt, w0, b0, T=1000, tol=0.5, alpha=0.25, lam1=1e-3, lam2=0.1, num_classes=10)
     corruption_log_reg.optimization()
 
     w = np.concatenate((corruption_log_reg.w, np.array([corruption_log_reg.b - np.mean(corruption_log_reg.b)])), axis=0)
